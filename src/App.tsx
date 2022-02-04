8a4a0fec8da607a5ac49b6a6e9232d1403957546
R8BY <vansosnova@mail.ru>
2022-02-04 13:44:38 +0300

    typescript todo list, setting up my project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import {
+    BrowserRouter,
+    Routes,
+    Route
+} from "react-router-dom";
+import {HomePage} from "./Pages/HomePage";
+import {ItemsPage} from "./Pages/ItemsPage";
+import { BadPage } from "./Pages/404";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="container">
+            <div className="items">
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<HomePage/>}/>;
+                        <Route path={"todo"} element={<ItemsPage/>}/>;
+                        <Route path={"*"} element={<BadPage/>}/>;
+                    </Routes>
+                </BrowserRouter>
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Pages/ItemsPage/ItemsPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from "./ItemsPage.module.scss";
+
+const ItemsPage = () => {
+    return (
+        <div>
+            ItemsPage
+        </div>
+    );
+};
+
+export { ItemsPage };
